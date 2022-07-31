a05b64f5729e0e7fdde1ea345c5f3c1f0286e1e4
Gabriel Marinho <gsmarinho@id.uff.br>
2022-07-31 16:01:55 -0300

    feat(ADDlobo):iniciando

---
+++ adicionar-lobinho/DOM.js
@@ -0,0 +1,11 @@
+const btn = document.querySelector("#sendb")
+const nome = document.querySelector("#nametext")
+const anos = document.querySelector("#yeartext")
+const linkFoto = document.querySelector("#linktext")
+const descricao = document.querySelector("#descrtext")
+
+btn.addEventListener("click", () => {
+    if((nome.value.trim() == "")||(anos.value.trim() == "")||(linkFoto.value.trim() == "")||(descricao.value.trim() == "")){
+        alert("Nao deixe nada sem resposta!")
+    }
+})
