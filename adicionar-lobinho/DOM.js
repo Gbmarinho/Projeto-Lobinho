c03409950b52a8c13cf6dfb986fc79d8b5129796
Gabriel Marinho <gsmarinho@id.uff.br>
2022-07-31 17:03:27 -0300

    feat:criando um Objeto para add lobo

--- adicionar-lobinho/DOM.js
+++ adicionar-lobinho/DOM.js
@@ -5,10 +5,18 @@ const linkFoto = document.querySelector("#linktext")
 const descricao = document.querySelector("#descrtext")
 
 btn.addEventListener("click", () => {
+    var h = isNaN(anos.value)
     if((nome.value.trim() == "")||(anos.value.trim() == "")||(linkFoto.value.trim() == "")||(descricao.value.trim() == "")){
         alert("Nao deixe nada sem resposta!")
-    }else if(anos.value.trim()){
-        
+    }else if(h == true){
+        alert("A idade colocada esta errada")
+    }else{
+        var newLobo = new Object()
+        newLobo.nome = nome.value.trim()
+        newLobo.anos = anos.value.trim()
+        newLobo.linkFoto = linkFoto.value.trim()
+        newLobo.descricao = descricao.value.trim()
+        console.log(newLobo)
     }
     
 })
