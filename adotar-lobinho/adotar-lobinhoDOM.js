7bd8921f6cb225e9d3673fa159c1a0374f559858
Joao Vitor Marins <marinsjoao@id.uff.br>
2022-07-31 19:29:58 -0300

    feat(adotar-lobinho):adicionando dom

---
+++ adotar-lobinho/adotar-lobinhoDOM.js
@@ -0,0 +1,19 @@
+const btn = document.querySelector("#sendb")
+const adptnome = document.querySelector("#nametext")
+const adptanos = document.querySelector("#yeartext")
+const adptemail = document.querySelector("#emailtext")
+btn.addEventListener("click", () => {
+    var h = isNaN(adptanos.value)
+    if((adptnome.value.trim() == "")||(adptanos.value.trim() == "")||(adptemail.value.trim() == "")){
+        alert("Nao deixe nada sem resposta!")
+    }else if(h == true){
+        alert("A idade colocada esta errada")
+    }else{
+        var putBody = new Object()
+        putBody.adapter_name = adptnome.value.trim()
+        putBody.adapter_age = adptanos.value.trim()
+        putBody.adpter_email = adptemail.value.trim()
+        putBody.adopted = true
+    }
+    
+})
