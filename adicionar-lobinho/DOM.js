f1aa9530cff64901201068cc5a7eddfd821f28e7
Gabriel Marinho <gsmarinho@id.uff.br>
2022-07-31 18:45:36 -0300

    fix(ADDlobinho):mensagem erro

--- adicionar-lobinho/DOM.js
+++ adicionar-lobinho/DOM.js
@@ -6,17 +6,20 @@ const descricao = document.querySelector("#descrtext")
 
 btn.addEventListener("click", () => {
     var h = isNaN(anos.value)
+    var n = nome.value
+    var j = isNaN(nome.value)
     if((nome.value.trim() == "")||(anos.value.trim() == "")||(linkFoto.value.trim() == "")||(descricao.value.trim() == "")){
         alert("Nao deixe nada sem resposta!")
     }else if(h == true){
         alert("A idade colocada esta errada")
+    }else if((j != true) || ((n.length < 4)||(n.length > 60))){
+        alert("Nome invalido")
     }else{
         var newLobo = new Object()
-        newLobo.nome = nome.value.trim()
-        newLobo.anos = anos.value.trim()
-        newLobo.linkFoto = linkFoto.value.trim()
-        newLobo.descricao = descricao.value.trim()
+        newLobo.name = nome.value.trim()
+        newLobo.age = anos.value.trim()
+        newLobo.link_image = linkFoto.value.trim()
+        newLobo.description = descricao.value.trim()
         console.log(newLobo)
     }
-    
 })
